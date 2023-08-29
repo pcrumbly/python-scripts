866e23b6b5465a00378cac415c2a1248f55823f3
pcrumbly <prestoncrumbly@gmail.com>
2023-08-29 01:18:47 -0500

    ADD scraping each team page for information and squad

--- serieAteams.py
+++ serieAteams.py
@@ -36,3 +36,55 @@ for row in table.find_all("tr")[1:]:
 # Print the dictionary with team names and links
 for team, link in teams_dict.items():
     print(f"Team: {team}\nLink: {link}\n")
+
+
+
+
+# Import necessary libraries
+import pandas as pd
+
+# Loop through the teams and links in the dictionary
+for team, link in teams_dict.items():
+    # Send a GET request to the team's Wikipedia page
+    team_response = requests.get(link)
+    
+    # Parse the HTML content using BeautifulSoup
+    team_soup = BeautifulSoup(team_response.content, "html.parser")
+    
+    # Find the "infobox vcard" table for team details
+    infobox_table = team_soup.find("table", class_="infobox vcard")
+    
+    # Initialize a dictionary to store team details
+    team_details = {}
+    
+    # Extract team details from the infobox table
+    for row in infobox_table.find_all("tr"):
+        header = row.find("th")
+        if header:
+            key = header.text.strip()
+            value = row.find("td").text.strip()
+            team_details[key] = value
+    
+    # Find the "wikitable football-squad nogrid" table for player information
+    squad_table = team_soup.find("table", class_="wikitable football-squad nogrid")
+    
+    # Initialize a list to store player information
+    player_list = []
+    
+    # Extract player information from the squad table
+    for row in squad_table.find_all("tr")[1:]:
+        columns = row.find_all("td")
+        player_name = columns[2].text.strip()
+        player_position = columns[1].text.strip()
+        player_age = columns[0].text.strip()
+        player_list.append({"Name": player_name, "Position": player_position, "Age": player_age})
+    
+    # Create a DataFrame for player information
+    player_df = pd.DataFrame(player_list)
+    
+    # Print team details and player information
+    print(f"Team: {team}")
+    print("Team Details:", team_details)
+    print("Player Information:")
+    print(player_df)
+    print("\n")
