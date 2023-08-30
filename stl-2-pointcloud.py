53833967d89d5e160a066b08f2c30f0f581a3619
pcrumbly <prestoncrumbly@gmail.com>
2023-08-30 02:49:51 -0500

    UPDATE try different conversion methods

--- stl-2-pointcloud.py
+++ stl-2-pointcloud.py
@@ -3,8 +3,33 @@ import tensorflow_graphics.geometry.representation.point as tfg_point
 import tensorflow_graphics.io as tfg
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import tensorflow_graphics.geometry as tfg_geo
 
-def convert_stl_to_point_cloud(stl_filename):
+def convert_stl_to_point_cloud(stl_filename, num_samples):
+    # Load the STL file as a mesh
+    stl_mesh = tfg.triangle_mesh.load(stl_filename)
+    
+    # Sample points from the mesh
+    sampled_points = tfg_point.sample_uniformly(stl_mesh.vertices, num_samples)
+    
+    # Convert the sampled points to a point cloud tensor
+    point_cloud = tf.convert_to_tensor(sampled_points, dtype=tf.float32)
+    
+    return point_cloud
+
+def convert_stl_to_point_cloud_2(stl_filename, num_samples):
+    # Load the STL file as a mesh
+    stl_mesh = tfg.triangle_mesh.load(stl_filename)
+    
+    # Sample points from the mesh
+    sampled_points = tfg_geo.representation.mesh.sampler.weighted_random_sample_triangle_mesh(stl_mesh.vertices, num_samples)
+    
+    # Convert the sampled points to a point cloud tensor
+    point_cloud = tf.convert_to_tensor(sampled_points, dtype=tf.float32)
+    
+    return point_cloud
+
+def convert_stl_to_point_cloud_3(stl_filename):
     # Load the STL file as a mesh
     stl_mesh = tfg.triangle_mesh.load(stl_filename)
     
@@ -33,7 +58,7 @@ def main():
     stl_filename = "stl/3DBenchy.stl"
     
     # Convert STL to point cloud
-    point_cloud = convert_stl_to_point_cloud(stl_filename)
+    point_cloud = convert_stl_to_point_cloud_2(stl_filename)
     
     # Visualize the point cloud
     visualize_point_cloud(point_cloud)
